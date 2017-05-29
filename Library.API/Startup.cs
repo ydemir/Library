a0c3b8e4751b5a6a10136d6e6dd22b8fe0e08163
Yusuf Demir <yusuf@bilgiedin.net>
2017-05-30 00:52:33 +0300

    Core Build Tamamlandı.

--- Library.API/Startup.cs
+++ Library.API/Startup.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Library.API.Entities;
 
 namespace Library.API
 {
@@ -27,6 +25,7 @@ namespace Library.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<LibraryContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:Default"]));
             // Add framework services.
             services.AddMvc();
         }
