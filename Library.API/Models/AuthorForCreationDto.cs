6e3cb271e416fbd67d15b54d15e498f9309baeaa
Yusuf Demir <yusuf@bilgiedin.net>
2017-06-02 03:33:02 +0300

    32 tamamlandı. Create child resource with parent resource  yazar ile birlikte list şeklinde kitaplarıda oluşturduk.

--- Library.API/Models/AuthorForCreationDto.cs
+++ Library.API/Models/AuthorForCreationDto.cs
@@ -12,5 +12,8 @@ namespace Library.API.Models
         public DateTimeOffset DateOfBirth { get; set; }
         public string Genre { get; set; }
 
+        public ICollection<BookForCreationDto> Books { get; set; }
+        = new List<BookForCreationDto>();
+
     }
 }
