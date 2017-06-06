747e2948c7752a84931d675269da9461cf291acf
Yusuf Demir <yusuf@bilgiedin.net>
2017-06-06 03:24:02 +0300

    33 tamamlandı Creating a collection of resources

---
+++ Library.API/Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Library.API.Entities;
+using Library.API.Models;
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authorcollections")]
+    public class AuthorCollectionsController:Controller
+    {
+        private ILabraryRepository _libraryRepository;
+
+        public AuthorCollectionsController(ILabraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        [HttpPost]
+        public IActionResult CreateAuthorCollection([FromBody] IEnumerable<AuthorForCreationDto> authorCollection)
+        {
+            if (authorCollection==null)
+            {
+                return BadRequest();
+            }
+
+            var authorEntities = Mapper.Map<IEnumerable<Author>>(authorCollection);
+
+            foreach (var author in authorEntities)
+            {
+                _libraryRepository.AddAuthor(author);
+            }
+
+            if (!_libraryRepository.Save())
+            {
+                throw new Exception("Creating an author collection failed on save.");
+            }
+
+            return Ok();
+        }
+    }
+}
