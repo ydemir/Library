f95fe1f0dbd5f24231dcdaa08d6d6197f8c1bb42
Yusuf Demir <yusuf@bilgiedin.net>
2017-06-06 23:26:45 +0300

    35 tamamlandı Handling post to a single resource

--- Library.API/Controllers/AuthorsController.cs
+++ Library.API/Controllers/AuthorsController.cs
@@ -2,6 +2,7 @@
 using Library.API.Entities;
 using Library.API.Models;
 using Library.API.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -61,6 +62,16 @@ namespace Library.API.Controllers
             return CreatedAtRoute("GetAuthor",
                 new { id = authorToReturn.Id },
                 authorToReturn);
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult BlockAuthorCreation(Guid id)
+        {
+            if (_libraryRepository.AuthorExists(id))
+            {
+                return new StatusCodeResult(StatusCodes.Status409Conflict);
+            }
+            return NotFound();
         }
     }
 }
